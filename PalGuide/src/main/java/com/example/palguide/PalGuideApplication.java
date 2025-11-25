6110ae4b50dbd730fd3c89339d196b561aa9882a
Amr <amr20012200131@hotmail.com>
2025-11-25 14:30:08 +0200

    Initial Commit

---
+++ PalGuide/src/main/java/com/example/palguide/PalGuideApplication.java
@@ -0,0 +1,13 @@
+package com.example.palguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PalGuideApplication.class, args);
+    }
+
+}
---
+++ PalGuide/src/test/java/com/example/palguide/PalGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.palguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalGuideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
