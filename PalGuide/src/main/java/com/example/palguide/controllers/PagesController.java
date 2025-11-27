193fe90615be2abb571a86179afd204b6f535a63
Amr <amr20012200131@hotmail.com>
2025-11-27 04:48:46 +0200

    uploaded employee dashboard

--- PalGuide/src/main/java/com/example/palguide/controllers/PagesController.java
+++ PalGuide/src/main/java/com/example/palguide/controllers/PagesController.java
@@ -1,12 +1,14 @@
 package com.example.palguide.controllers;
 
 import com.example.palguide.common.Models.User;
+import com.example.palguide.common.enums.Role;
 import com.example.palguide.services.UserService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
@@ -51,5 +53,23 @@ public class PagesController {
     }
 
 
+    @GetMapping("/request")
+    public String request() {
+        return "requestService.html";
+    }
+
+    @GetMapping("/dash")
+    public String dash(HttpSession session, RedirectAttributes redirectAttributes) {
+        if(session.getAttribute("user_id") == null) {
+            redirectAttributes.addFlashAttribute("message", "You are not logged in!");
+            return "redirect:/login";
+        }
+        User user = userService.getUserById((Long)session.getAttribute("user_id"));
+        if(user.getRole() != Role.Admin){
+            redirectAttributes.addFlashAttribute("message", "You are not allowed to access this page!");
+            return "redirect:/home";
+        }
+        return "employee_dashboard.html";
+    }
 
 }
