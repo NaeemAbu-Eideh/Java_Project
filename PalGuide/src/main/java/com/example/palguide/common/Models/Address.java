128ad8ccce2419796785febb896fbc8a030e5820
Amr <amr20012200131@hotmail.com>
2025-11-28 20:33:55 +0200

    quick fix

--- PalGuide/src/main/java/com/example/palguide/common/Models/Address.java
+++ PalGuide/src/main/java/com/example/palguide/common/Models/Address.java
@@ -1,5 +1,6 @@
 package com.example.palguide.common.Models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -42,6 +43,7 @@ public class Address {
 
     @Setter
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JsonIgnore
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
